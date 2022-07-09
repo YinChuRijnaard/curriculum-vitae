c8348b6288a90ca847d8704242aa943580872a1b
Yin Chu <ycrijnaard@gmail.com>
2022-07-09 18:32:23 +0200

    Edit in Intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,7 +3,8 @@ export const Intro = () => {
     <div className="mx-4 my-4 rounded border border-neutral-600 p-4 leading-relaxed">
       <p>
         <span className="font-bold text-purple-600">Passionate</span>&nbsp;about
-        everything frontend.&nbsp;
+        everything frontend. Proficient in writing&nbsp;
+        <span className="font-bold text-purple-600">clean code</span>.&nbsp;
         <span className="font-bold text-purple-600">Dedicated</span>
         &nbsp;to learning the trade to the best of my abilities, while using any
         tool necessary to get the job done.&nbsp;
